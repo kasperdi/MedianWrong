11e4c5db3d91a5631a90a8e72d6f9dd8f5aa4952
Kasper Ilsøe <kasperdi1412@gmail.com>
2018-10-29 12:07:31 +0100

    Initial sharing of project

---
+++ Median.java
@@ -0,0 +1,29 @@
+/**
+ * Class to be debugged and tested
+ * 
+ * @author Kurt Jensen
+ * @version Oct 2017-10-17
+ */
+public class Median {
+
+    /** 
+     * Returns the median of the three parameters (i.e. the numerical middle).
+     * The method contains a bug, and hence it sometimes returns a wrong result
+     */
+    public static int medianWrong(int x, int y, int z) {
+        int m = z;
+        if(y < z) {
+            if (x < y) { m = y; }
+            else {
+                if (x < z) { m = y; }
+            }
+        }
+        else {
+            if(x > y) { m = y; }
+            else {
+                if (x > z) { m = x; }
+            } 
+        }
+        return m;
+    }
+}
